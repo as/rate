51a9c21a41729707e34aca7af81aef4053f6f0e6
as <as.utf8@gmail.com>
2018-05-13 00:06:52 -0700

    gofmt -s

--- lim.go
+++ lim.go
@@ -1,18 +1,18 @@
-// Package rate provides a very simple rate limiter based on the passage of time. 
+// Package rate provides a very simple rate limiter based on the passage of time.
 package rate
 
 import (
 	"time"
 )
 
-const(
-	tickInterval = time.Second*3
+const (
+	tickInterval    = time.Second * 3
 	preallocEntries = 64
-	maxSweep = 10
+	maxSweep        = 10
 )
 
-// Limiter provides a way to schedule named tasks for execution. 
-type Limiter interface{
+// Limiter provides a way to schedule named tasks for execution.
+type Limiter interface {
 	// Quantum returns the duration allocated for every named task. This value is a
 	// limiter-scoped, maximum-duration watermark. It does not represent the quantum
 	// available for a specific task.
@@ -20,37 +20,37 @@ type Limiter interface{
 	// If the Limiter is a aggregate of multiple Limiters, Quantum() should return the smallest
 	// time.Duration in the aggregate.
 	Quantum() time.Duration
-	
+
 	// Schedule schedules the task to run for the given time slice if there is quantum
-	// available for that task. 
+	// available for that task.
 	//
-	// If the delay is <= 0 the task can run immediately and the time slice provided 
+	// If the delay is <= 0 the task can run immediately and the time slice provided
 	// is subtracted from the task's quantum. If delay is > 0, the caller may wait the delay
 	// and attempt to schedule the task again, otherwise the task should be abandoned.
 	Schedule(task string, slice time.Duration) (delay time.Duration)
-	
+
 	// Close closes the limiter
 	Close() error
 }
 
 // AllowSlice returns true if task may execute for 1s at time.Now()
-func Allow(l Limiter, task string) bool{
+func Allow(l Limiter, task string) bool {
 	return l.Schedule(task, time.Second) <= 0
 }
 
 // AllowSlice returns true if task may execute for the slice duration at time.Now()
-func AllowSlice(l Limiter, task string, slice time.Duration) bool{
+func AllowSlice(l Limiter, task string, slice time.Duration) bool {
 	return l.Schedule(task, slice) <= 0
 }
 
 // New returns a limiter that allows task to run for the specified quantum
-// Calls to Allow and AllowSlice reduce a task's available quantum if that 
+// Calls to Allow and AllowSlice reduce a task's available quantum if that
 // task is allowed to run. The quantum is replenished naturally via the passage
 // of time.
 func New(quantum time.Duration) *limiter {
 	l := &limiter{
-		quantum:   quantum,
-		schedule:       make(chan ask, 1),
+		quantum:  quantum,
+		schedule: make(chan ask, 1),
 		closecap: make(chan bool, 1),
 		done:     make(chan bool),
 	}
@@ -61,8 +61,8 @@ func New(quantum time.Duration) *limiter {
 
 // limiter is a rate limiter
 type limiter struct {
-	quantum         time.Duration
-	schedule	chan ask
+	quantum        time.Duration
+	schedule       chan ask
 	closecap, done chan bool
 }
 
@@ -73,12 +73,12 @@ func (l *limiter) Schedule(task string, slice time.Duration) (delay time.Duratio
 	l.schedule <- ask{
 		string:   task,
 		Duration: slice,
-		reply: reply,
+		reply:    reply,
 	}
 	return <-reply
 }
 
-func (l *limiter) Quantum() time.Duration{
+func (l *limiter) Quantum() time.Duration {
 	return l.quantum
 }
 
@@ -98,10 +98,10 @@ func (l *limiter) Close() error {
 func (l *limiter) run() {
 	m := make(map[string]time.Time, preallocEntries)
 	tick := time.NewTicker(tickInterval)
-	
+
 	defer close(l.schedule)
 	defer tick.Stop()
-	
+
 	for {
 		select {
 		case ask := <-l.schedule:
@@ -109,16 +109,16 @@ func (l *limiter) run() {
 			then := l.floor(m[ask.string], now).Add(ask.Duration)
 			delta := then.Sub(now)
 			ask.reply <- delta
-			if delta <= 0{
+			if delta <= 0 {
 				m[ask.string] = then
 			}
 		case <-tick.C:
-			select{
+			select {
 			case <-l.done:
 				return
 			default:
 			}
-			
+
 			// TODO(as): The best number is probably not the current MaxSweep
 			i := 0
 			t := time.Now()
@@ -136,7 +136,7 @@ func (l *limiter) run() {
 }
 
 // floor returns the mark time clamped to [now-window, +inf)
-func (l *limiter) floor(mark time.Time, now time.Time) time.Time{
+func (l *limiter) floor(mark time.Time, now time.Time) time.Time {
 	if t := now.Add(-l.quantum); !mark.After(t) {
 		return t
 	}
--- lim_test.go
+++ lim_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestLimiterBasic(t *testing.T) {
-	l := New(time.Second*30)
+	l := New(time.Second * 30)
 	n := 0
 	for ; n < 100000; n++ {
 		if !Allow(l, "bar") {
@@ -21,44 +21,44 @@ func TestLimiterBasic(t *testing.T) {
 }
 
 func TestLimiterSchedule(t *testing.T) {
-	l := New(time.Second*2)
-	Allow(l,"a")
-	Allow(l,"a")
+	l := New(time.Second * 2)
+	Allow(l, "a")
+	Allow(l, "a")
 	delay := l.Schedule("a", time.Second)
-	if delay.Truncate(time.Second) != time.Second{
+	if delay.Truncate(time.Second) != time.Second {
 		t.Fatalf("bad delay: want ~1s, have %s", delay)
 	}
 }
 
 func TestLimiterReplenish(t *testing.T) {
-	l := New(time.Second*3)
-	Allow(l,"bar")
-	Allow(l,"bar")
-	Allow(l,"bar")
-	Allow(l,"bar")
-	Allow(l,"bar")
-	Allow(l,"bar")
-	if Allow(l,"bar") {
+	l := New(time.Second * 3)
+	Allow(l, "bar")
+	Allow(l, "bar")
+	Allow(l, "bar")
+	Allow(l, "bar")
+	Allow(l, "bar")
+	Allow(l, "bar")
+	if Allow(l, "bar") {
 		t.Fatalf("1/3: have allow, want deny")
 	}
 	time.Sleep(time.Second)
-	if !Allow(l,"bar") {
+	if !Allow(l, "bar") {
 		t.Fatalf("2/3: have deny, want allow")
 	}
-	if Allow(l,"bar") {
+	if Allow(l, "bar") {
 		t.Fatalf("3/3: have allow, want deny")
 	}
 }
 
 func TestLimiterMulti(t *testing.T) {
-	l := New(time.Second*30)
+	l := New(time.Second * 30)
 	for i := 0; i < 300; i++ {
 		go Allow(l, "baz")
 	}
 
 	n := 0
 	for ; n < 100000; n++ {
-		if !Allow(l,"bar") {
+		if !Allow(l, "bar") {
 			break
 		}
 	}
@@ -68,11 +68,11 @@ func TestLimiterMulti(t *testing.T) {
 }
 
 func BenchmarkLimiter(b *testing.B) {
-	l := New(time.Second*30)
+	l := New(time.Second * 30)
 	body := func(pb *testing.PB) {
 		name := fmt.Sprint(rand.Int31n(7))
 		for pb.Next() {
-			Allow(l,name)
+			Allow(l, name)
 		}
 	}
 	b.RunParallel(body)
